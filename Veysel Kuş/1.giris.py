9fa7acdde2657ce991ddd54dfe8ebf16145cfbc4
Veysel KUŞ <kusveysel00@gmail.com>
2026-07-20 15:10:27 +0300

    20/07/2026

---
+++ Veysel Kuş/1.giris.py
@@ -0,0 +1,27 @@
+
+#? çıktı vermek için kullanılır
+print ("Python Giris")
+
+#? değişkene atanan değer, değişkenin tipi olur
+a = 1
+a = "veysel"
+print (a)
+
+#? casting (tip belirleme)
+x = str(7)
+y = int(7)
+z = float(7)
+
+print(x, y ,z)
+
+#? type() değişkenin tipini gösterir
+print(type(x),type(y),type(z))
+
+#? değişken oluşturma
+#* değişkenler sadece büyük/küçük harfler ,sayılar ve _ işareti ile oluşturulabilir ve bir değişken sayı ile başlayamaz
+
+#? variable unpacking
+colors=["white","red","grey","red","yellow"]
+a,b,c,d,e=colors #! Destruct işlemidir ,React'ta vs. yaygın olarak kullanılır 
+print(a,b,c,d,e)
+
